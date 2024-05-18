2fb9e96751a2489618ea08c53b49b16fcb24e7a3
B33blebroxx <b33blebroxx@gmail.com>
2024-05-18 11:12:56 -0500

    Added Models

---
+++ Models/Performer.cs
@@ -0,0 +1,15 @@
+﻿namespace IndieWorld.Models
+{
+    public class Performer
+    {
+        public int Id { get; set; }
+        public string? RingName { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Bio { get; set; }
+        public string? Hometown { get; set; }
+        public string? Accolades { get; set; }
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+        public ICollection<Show>? Shows { get; set; }
+    }
+}
---
+++ Models/Promotion.cs
@@ -0,0 +1,14 @@
+﻿namespace IndieWorld.Models
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+        public string PromotionName { get; set; }
+        public string Acronym { get; set; }
+        public string LogoUrl { get; set; }
+        public string HomeCity { get; set; }
+        public int Established { get; set; }
+        public string Owner { get; set; }
+        public string ShowFrequency { get; set; }
+    }
+}
