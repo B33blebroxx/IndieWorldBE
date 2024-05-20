129628025d05ed0f4e3394f3916168317ba51123
B33blebroxx <b33blebroxx@gmail.com>
2024-05-20 14:20:31 -0500

    Check User and Register User calls working

---
+++ API/UserApi.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IndieWorld.Models;
+
+namespace IndieWorld.API
+{
+    public class UserApi
+    {
+        public static void Map(WebApplication app)
+        {
+            //Check User
+            app.MapGet("/checkUser/{uid}", (IndieWorldDbContext db, string uid) =>
+            {
+                var user = db.Users.FirstOrDefault(u => u.Uid == uid);
+
+                if (user == null)
+                {
+                    return Results.NotFound("");
+                }
+
+                return Results.Ok(user);
+            });
+
+            //Register User
+            app.MapPost("/users/register", (IndieWorldDbContext db, User newUser) =>
+            {
+                try
+                {
+                    db.Users.Add(newUser);
+                    db.SaveChanges();
+                    return Results.Created($"/users/{newUser.Id}", newUser);
+                }
+                catch (DbUpdateException)
+                {
+                    return Results.BadRequest("Unable to register user");
+                }
+            });
+        }
+    }
+}
