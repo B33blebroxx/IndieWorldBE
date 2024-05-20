6c18229558d7efe9c962ec5c0eb47e3a69f9930d
B33blebroxx <b33blebroxx@gmail.com>
2024-05-20 18:53:36 -0500

    Added get all roles call

---
+++ Controllers/RoleApi.cs
@@ -0,0 +1,20 @@
+﻿namespace IndieWorld.Controllers
+{
+    public class RoleApi
+    {
+        public static void Map(WebApplication app)
+        {
+            //Get all Roles
+            app.MapGet("/roles", (IndieWorldDbContext db) =>
+            {
+                var roles = db.Roles.ToList();
+                if (roles == null)
+                {
+                    return Results.NoContent();
+                }
+
+                return Results.Ok(roles);
+            });
+        }
+    }
+}
